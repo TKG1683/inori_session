5fe4c6329df41bbe9aaecc704a9c48f919550c6a
TKG1683 <h.takagi.1683@icloud.com>
2024-07-13 02:51:11 +0900

    ページスタイルを変更

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,8 @@
-// インタラクティブ機能をここに追加
+function toggleMenu() {
+    const nav = document.getElementById('overlay-nav');
+    if (nav.style.display === 'block') {
+        nav.style.display = 'none';
+    } else {
+        nav.style.display = 'block';
+    }
+}
