163e4a6c01e6b6c45c4d005c81c851303746a348
TKG1683 <h.takagi.1683@icloud.com>
2025-02-24 22:52:14 +0900

    Add member icons and implement member navigation in vol4 page

--- src/vol4/04_vol4_members.jsx
+++ src/vol4/04_vol4_members.jsx
@@ -1,74 +1,111 @@
 import PropTypes from "prop-types";
 import styles from "./04_vol4_members.module.css";
+import { useState } from "react";
 
 const Members = ({ className = "" }) => {
+  const members = [
+    {
+      name: "TKG",
+      part: "Drums",
+      favoriteSongs: "旅の途中、三月と群青、Well Wishing Word",
+      comment: "まさかのドラムワンマン運転！がんばります…！",
+      image: "./images/member_icon/tkg-icon.jpg",
+    },
+    {
+      name: "Gensan",
+      part: "Bass, Guitar",
+      favoriteSongs: "REAL-EYES、約束のアステリズム、brave climber",
+      comment: "いのりの涙を受け止めるために、全力で演奏します。",
+      image: "./images/member_icon/gensan-icon.jpg",
+    },
+    {
+      name: "親フラ",
+      part: "Guitar",
+      favoriteSongs: "クリスタライズ、Kitty Cat Adventure",
+      comment: "速弾きします。",
+      image: "./images/member_icon/ParentFlash-icon.jpg",
+    },
+  ];
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handleNext = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % members.length);
+  };
+
+  const handleBack = () => {
+    setCurrentIndex((prevIndex) => (prevIndex - 1 + members.length) % members.length);
+  };
+
   return (
-    <div className={[styles.memberContainer, className].join(" ")}>
-      <div className={styles.memberInfo}>
-        <img
-          className={styles.fukidashi032Icon}
-          loading="lazy"
-          alt=""
-          src="./images/fukidashi.png"
-        />
-        <h3 className={styles.members1}>members</h3>
-      </div>
-      <div className={styles.memberProfiles}>
-        <div className={styles.navigationButtons}>
-          <div className={styles.arrowBackWrapper}>
+    <div className={[styles.membersContainer, className].join(" ")}>
+      <div className={styles.membersChild}>
+        <div className={styles.membersSubTitle}>
+          <img
+            className={styles.fukidashiImage}
+            loading="lazy"
+            alt=""
+            src="./images/fukidashi.png"
+          />
+          <div className={styles.membersText}>members</div>
+        </div>
+
+        <div className={styles.membersMain}>
+          <img className={styles.kurariCamvasImage} loading="lazy" alt="" src="./images/kurari_canvas.png" />
+          <div className={styles.navigationButtons}>
             <img
               className={styles.arrowBackIcon}
               loading="lazy"
               alt=""
               src="./images/arrow-back.svg"
+              onClick={handleBack}
             />
-          </div>
-          <div className={styles.profileContainer}>
-            <div className={styles.profileCard}>
-              <div className={styles.profileContent}>
-                <div className={styles.memberImageContainer}>
-                  <img
-                    className={styles.tkgImgIcon}
-                    loading="lazy"
-                    alt=""
-                    src="./images/member_icon/tkg-icon.png"
-                  />
-                </div>
-                <div className={styles.div}>
-                  <p className={styles.nametkg}>
-                    <span>name：</span>
-                    <span className={styles.tkg}>TKG</span>
-                  </p>
-                  <p className={styles.nametkg}>
-                    <span>part：</span>
-                    <span className={styles.tkg}>Drums</span>
-                    <span className={styles.span}>{`  `}</span>
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div className={styles.div1}>
-              <p className={styles.nametkg}>favorite songs:</p>
-              <p className={styles.nametkg}>
-                　旅の途中、三月と群青、Well Wishing Word
-              </p>
-              <p className={styles.nametkg}>&nbsp;</p>
-              <p className={styles.nametkg}>commnet:</p>
-              <p className={styles.nametkg}>　まさかのドラムワンマン運転！</p>
-              <p className={styles.nametkg}>　がんばります…！</p>
-            </div>
-          </div>
-          <div className={styles.arrowBackWrapper}>
             <img
-              className={styles.arrowBackIcon}
+              className={styles.arrowNextIcon}
               loading="lazy"
               alt=""
               src="./images/arrow-next.svg"
+              onClick={handleNext}
             />
+            <div className={styles.descriptionMemberInfo}>
+              <div className={styles.profileContainer}>
+                <div className={styles.profileCard}>
+                  <div className={styles.profileContent}>
+                    <div className={styles.memberImageContainer}>
+                      <img
+                        className={styles.tkgImgIcon}
+                        loading="lazy"
+                        alt=""
+                        src={members[currentIndex].image}
+                      />
+                    </div>
+                    <div className={styles.div}></div>
+                      <p className={styles.nametkg}>
+                        <span>name：</span>
+                        <span className={styles.tkg}>{members[currentIndex].name}</span>
+                      </p>
+                      <p className={styles.nametkg}>
+                        <span>part：</span>
+                        <span className={styles.tkg}>{members[currentIndex].part}</span>
+                        <span className={styles.span}>{`  `}</span>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className={styles.div1}>
+                  <p className={styles.nametkg}>favorite songs:</p>
+                  <p className={styles.nametkg}>
+                    {members[currentIndex].favoriteSongs}
+                  </p>
+                  <p className={styles.nametkg}>&nbsp;</p>
+                  <p className={styles.nametkg}>comment:</p>
+                  <p className={styles.nametkg}>{members[currentIndex].comment}</p>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
   );
 };
 
